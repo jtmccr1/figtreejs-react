14bb47c1696311e9d59d76f64a969b7d17905996
jtmccr1 <mccronejt@gmail.com>
2020-02-10 22:52:25 +0000

    better regex if such a thing exists

--- src/utils/immutableTree.js
+++ src/utils/immutableTree.js
@@ -11,38 +11,19 @@ export default class ImmutableTree{
 
         //strip first and last parenthesis and annotations ect. call again on children.
 //https://www.regextester.com/103043
-        const initialRegex =/\((.*)\)(.*)/gm;
-        const childrenCommas=/,(?=(?:(?:(?!\)).)*\()|[^\(\)]*$)/g;
-        const match = initialRegex.exec(newickString);
+        const internalNode =/\((.*)\)(.*)/;
+        const exposedCommas=/,(?=(?:(?:(?![\)]).)*[\(])|[^\(\)]*$)(?=(?:(?:(?![\]]).)*[\[])|[^\[\]]*$)(?=(?:(?:(?![\}]).)*[\{])|[^\{\{]*$)/g;
+        const annotations = /(.*?)(?:\[&?(.*)\])*(?:#(.+))*:(\d*\.?\d*)/g
 
-        // if not match check for tip signiture (.*):(.*)
+        if(internalNode.test(newickString)){
+            const [childrenString,data] = newickString.split(internalNode).filter(s=>s);
+            const children = childrenString.split(exposedCommas);
+            console.log(children)
 
-        // if(match){
-            const [childrenString,data] = match.slice(1,3);
-            const splits = childrenCommas.exec(childrenString);
-        let match2
-        while ((match2 = childrenCommas.exec(childrenString)) !=null) {
-            console.log("match found at " + match.index);
-
-            }
-            // const children = splits.map((d,i)=>{
-            //     if(i===0){
-            //         return childrenString.substring(0,d.index)
-            //     }else{
-            //         return childrenString.substring(splits[i-1].index,d.index)
-            //     }
-            // });
-
-
-
-
-            //split children and process; can check for tip here
-            // console.log(children);
-            // process data here
-            console.log(data)
-        // }
-
-
+            console.log(data.split(annotations))
+        }else{
+            // it's a tip parse for data.
+        }
 
 
 
