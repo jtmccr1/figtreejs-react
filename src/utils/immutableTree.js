6bedf9340df822e403b25cfc0bf8067ad81351ef
JT <mccrone@umich.edu>
2020-02-11 09:06:01 +0000

    working on parser

--- src/utils/immutableTree.js
+++ src/utils/immutableTree.js
@@ -7,33 +7,55 @@ export default class ImmutableTree{
     // Set node number in alphabetical order
 
     static parseNewick(newickString, options={}) {
-        options ={...{labelName: "label",datePrefix:undefined,dateFormat:"decimal"},...options}
+        options ={...{labelName: "label",datePrefix:undefined,dateFormat:"decimal"},...options};
 
-        //strip first and last parenthesis and annotations ect. call again on children.
+        let nodeCount=0;
+
+        function newickSubstringParser(newickString){
+            // check for semicolon
+            //strip first and last parenthesis and annotations ect. call again on children.
 //https://www.regextester.com/103043
-        const internalNode =/\((.*)\)(.*)/;
-        const exposedCommas=/,(?=(?:(?:(?![\)]).)*[\(])|[^\(\)]*$)(?=(?:(?:(?![\]]).)*[\[])|[^\[\]]*$)(?=(?:(?:(?![\}]).)*[\{])|[^\{\{]*$)/g;
-        const annotations = /(.*?)(?:\[&?(.*)\])*(?:#(.+))*:(\d*\.?\d*)/g
+            //                      [children]data - name, label, branch length ect.
+            const internalNode =/\((.*)\)(.*)/;
+            // identify commas not included in (),[],or {}
+            const exposedCommas=/,(?=(?:(?:(?![\)]).)*[\(])|[^\(\)]*$)(?=(?:(?:(?![\]]).)*[\[])|[^\[\]]*$)(?=(?:(?:(?![\}]).)*[\{])|[^\{\{]*$)/g;
+            const nodeData = /(.*?)(?:\[&?(.*)\])*(?:#(.+))*:(\d*\.?\d*)/g
 
-        if(internalNode.test(newickString)){
-            const [childrenString,data] = newickString.split(internalNode).filter(s=>s);
-            const children = childrenString.split(exposedCommas);
-            console.log(children)
+            const isInternalNode = internalNode.test(newickString);
+            let nodeString,
+                childrenString,
+                childNodes=[];
+            if(isInternalNode){
+                [childrenString,nodeString] = newickString.split(internalNode).filter(s=>s);
+                const children = childrenString.split(exposedCommas);
+                for(const child of children){
+                   childNodes=childNodes.concat(newickSubstringParser(child))
+                }
+            }else{
+                //Add tip to tip list
+                nodeString = newickString;
+            }
 
-            console.log(data.split(annotations))
-        }else{
-            // it's a tip parse for data.
+            //TODO get rid of leading and trailing empty matches
+            const [emptyMatch,name,annotations,label,length,emptyMatch2]=nodeString.split(nodeData);
+
+            const node = {
+                id:name?name:label?label:(`node${(nodeCount+=1)}`),
+                name:name?name:null,
+                label:label?label:null,
+                length:length!==undefined?parseInt(length):1,
+                annotations:annotations?annotations:null,
+                children:childNodes.length>0?childNodes.map(n=>n.id):null,
+            };
+
+            return {[node.id]:node,...childNodes}
+
         }
-
-
-
-        const node = {}
-
-
-
+        return newickSubstringParser(newickString);
     }
 
 
 
 
 }
+
--- tests/immutableTree.test.js
+++ tests/immutableTree.test.js
@@ -2,7 +2,9 @@ import ImmutableTree from "../src/utils/immutableTree";
 
 describe("Tree Tests",()=>{
     it("parse newick",()=>{
-        const treeString="((A:1,B:2):3,C:4,D242[&length_range={2.9997859591901488E-6,0.15966713321452264},location=\"Mabalako\",location.prob=1.0,location.set={\"Mabalako\"},8630136985685]:4):120;"
+        const treeString="((A:1,B:2):3,C:4,D242[&length_range={2.9997859591901488E-6,0.15966713321452264},location=\"Mabalako\",location.prob=1.0,location.set={\"Mabalako\"},8630136985685]:4)[&a={1}]):120;"
         const tree = ImmutableTree.parseNewick(treeString);
+
+        console.log(tree);
     })
-})
+});
