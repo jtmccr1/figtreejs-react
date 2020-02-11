edcb080e14bed3a77c5cdec24631669d5273f4d1
JT <mccrone@umich.edu>
2020-02-11 16:59:16 +0000

    newick parser there some todos

--- src/utils/immutableTree.js
+++ src/utils/immutableTree.js
@@ -1,3 +1,6 @@
+import {Type} from "./tree";
+import {extent} from "d3-array";
+
 export default class ImmutableTree{
     constructor(nodes){
         this.nodes = {};
@@ -19,8 +22,7 @@ export default class ImmutableTree{
             const internalNode =/\((.*)\)(.*)/;
             // identify commas not included in (),[],or {}
             const exposedCommas=/,(?=(?:(?:(?![\)]).)*[\(])|[^\(\)]*$)(?=(?:(?:(?![\]]).)*[\[])|[^\[\]]*$)(?=(?:(?:(?![\}]).)*[\{])|[^\{\{]*$)/g;
-            const nodeData = /(.*?)(?:\[&?(.*)\])*(?:#(.+))*:(\d*\.?\d*)/g
-
+            const nodeData = /(.*?)(?:\[&?(.*)\])*(?:#(.+))*:(\d*\.?\d*)/g;
             const isInternalNode = internalNode.test(newickString);
             let nodeString,
                 childrenString,
@@ -35,27 +37,234 @@ export default class ImmutableTree{
                 //Add tip to tip list
                 nodeString = newickString;
             }
-
             //TODO get rid of leading and trailing empty matches
-            const [emptyMatch,name,annotations,label,length,emptyMatch2]=nodeString.split(nodeData);
-
+            const [emptyMatch,name,annotationsString,label,length,emptyMatch2]=nodeString.split(nodeData);
             const node = {
                 id:name?name:label?label:(`node${(nodeCount+=1)}`),
                 name:name?name:null,
                 label:label?label:null,
-                length:length!==undefined?parseInt(length):1,
-                annotations:annotations?annotations:null,
-                children:childNodes.length>0?childNodes.map(n=>n.id):null,
-            };
+                length:length!==undefined?parseFloat(length):undefined,
+                children:childNodes.length>0?childNodes.map(n=> Object.keys(n.nodesById)[0]):null,
+        };
 
-            return {[node.id]:node,...childNodes}
+            const annotations = annotationsString!==undefined? parseAnnotation(annotationsString):{};
+            const typedAnnotations = typeAnnotations(annotations);
+            // TODO get clades
+            //TODO parse dates
+            //TODO strip names/labels ect of quotes
+            const nodesById = {[node.id]:node,...childNodes.reduce((acc,curr)=>({...acc,...curr.nodesById}),{})};
+            const annotationsById = {[node.id]:annotations,...childNodes.reduce((acc,curr)=>({...acc,...curr.annotationsById}),{})};
+            const annotationTypes =[typedAnnotations,...childNodes.map(child=>child.annotationTypes)].reduce((acc,curr)=>{return reconcileAnnotations(curr,acc)},{});
+
+
+            return ({nodesById,annotationsById,annotationTypes})
 
         }
         return newickSubstringParser(newickString);
     }
 
 
+}
 
+function parseAnnotation(annotationString){
+    const exposedCommas=/,(?=(?:(?:(?![\)]).)*[\(])|[^\(\)]*$)(?=(?:(?:(?![\]]).)*[\[])|[^\[\]]*$)(?=(?:(?:(?![\}]).)*[\{])|[^\{\}]*$)/g;
+    const setRegex = /\{(.+)\}/;
+    // const setRegex = /\{(.+)\}/;
+    const out = {};
+    for( const annotation of annotationString.split(exposedCommas)){
+
+        let [annotationKey,data] = annotation.split("=");
+            annotationKey=annotationKey.replace(".","_");
+            if(setRegex.test(data)) {
+                data = data.split(setRegex).filter(s => s !== "").reduce((acc,curr)=>acc.concat(curr.split(exposedCommas)),[]);
+                if (data.reduce((acc, curr) => (acc & !isNaN(curr)), true)) {
+                    data = data.map(d => parseFloat(d));
+                }else{
+                    data = data.reduce((acc,curr)=>acc.concat(curr.split(/[(?:\")')]/).filter(s=>s!=="")),[])
+                }
+                    out[annotationKey]=data;
+            }else{
+                data = data.split(/[(?:\")')]/).filter(s=>s!=="")[0] ;
+                if(isNaN(data)){
+                    out[annotationKey]=data
+                }else{
+                    out[annotationKey]=parseFloat(data);
+                }
+
+            }
+    }
+    return constructProbabilitySet(out)
 
 }
 
+function constructProbabilitySet(out){
+    const keys =Object.keys(out);
+    const finalObject ={};
+    const skippedKeys = [];
+    for(const probabilityKey of keys){
+        if(/.+_prob/.test(probabilityKey)){
+            const base = probabilityKey.split("_prob").filter(s=>s!=="");
+            const traitkey = `${base}_set`;
+            const probabilities=[].concat(out[probabilityKey]);
+            if(keys.includes(traitkey)) {
+                const probabilitySet = {};
+                for (let i = 0; i < out[traitkey].length; i++) {
+                    probabilitySet[out[traitkey][i]] = probabilities[i];
+                    finalObject[`${base}_probSet`] = {...probabilitySet};
+                }
+            }
+        }
+        finalObject[probabilityKey]=out[probabilityKey];
+    }
+    return finalObject;
+}
+function reconcileAnnotations(incomingAnnotations,currentAnnotations={}){
+    for (let [key, types] of Object.entries(incomingAnnotations)) {
+        let annotation = currentAnnotations[key];
+        if (!annotation) {
+            currentAnnotations[key] = types;
+        } else {
+            const type = types.type
+            if (annotation.type !== type) {
+                if ((type === Type.INTEGER && annotation.type === Type.FLOAT) ||
+                    (type === Type.FLOAT && annotation.type === Type.INTEGER)) {
+                    // upgrade to float
+                    annotation.type = Type.FLOAT;
+                    if (annotation.values) {
+                        delete annotation.values;
+                    }
+                    annotation.extent = annotation.extent ? extent(annotation.extent.concat(types.extent)) : types.extent
+                } else {
+                    throw Error(`existing values of the annotation, ${key}, in the tree is not of the same type`);
+                }
+            } else if (annotation.type === type) {
+                if (type === Type.DISCRETE) {
+                    if (!annotation.values) {
+                        annotation.values = new Set();
+                    }
+                    annotation.values = new Set([...annotation.values, ...types.values])
+                } else if (annotation.values || types.values) {
+                    annotation.values = annotation.values ? annotation.values.concat(types.values) : types.values
+                } else if (annotation.extent || types.extent) {
+                    annotation.extent = annotation.extent ? extent(annotation.extent.concat(types.extent)) : types.extent
+
+                }
+            }
+        }
+    }
+    return currentAnnotations;
+}
+
+function typeAnnotations(annotations){
+    let annotationTypes={};
+    for (let [key, addValues] of Object.entries(annotations)) {
+            const annotation = {};
+             annotationTypes[key] = annotation;
+
+        if (Array.isArray(addValues)) {
+            // is a set of  values
+            let type;
+            if(addValues.map(v=>isNaN(v)).reduce((acc,curr)=>acc&&curr,true)) {
+                type = Type.DISCRETE;
+                annotation.type = type;
+                if (!annotation.values) {
+                    annotation.values = new Set();
+                }
+                annotation.values.add(...addValues);
+            }else if(addValues.map(v=>parseFloat(v)).reduce((acc,curr)=>acc&&Number.isInteger(curr),true)){
+                type =Type.INTEGER;
+                annotation.extent=extent(addValues)
+            }else{
+                type = Type.FLOAT;
+                annotation.extent=extent(addValues)
+            }
+
+            if (annotation.type && annotation.type !== type) {
+                if ((type === Type.INTEGER && annotation.type === Type.FLOAT) ||
+                    (type === Type.FLOAT && annotation.type === Type.INTEGER)) {
+                    // upgrade to float
+                    type = Type.FLOAT;
+                    annotation.type = Type.FLOAT;
+                    if(annotation.values){
+                        delete annotation.values;
+                    }else{
+                        throw Error(`existing values of the annotation, ${key}, in the tree is discrete.`);
+                    }
+                }
+            }
+            annotation.type=type;
+            // annotation.values = annotation.values? [...annotation.values, ...addValues]:[...addValues]
+        } else if (Object.isExtensible(addValues)) {
+
+            // is a set of properties with values
+            let type = undefined;
+            let sum = 0.0;
+            let keys = [];
+            for (let [key, value] of Object.entries(addValues)) {
+                if (keys.includes(key)) {
+                    throw Error(`the states of annotation, ${key}, should be unique`);
+                }
+                if (typeof value === typeof 1.0) {
+                    // This is a vector of probabilities of different states
+                    type = (type === undefined) ? Type.PROBABILITIES : type;
+
+                    if (type === Type.DISCRETE) {
+                        throw Error(`the values of annotation, ${key}, should be all boolean or all floats`);
+                    }
+
+                    sum += value;
+                    if (sum > 1.01) {
+                        throw Error(`the values of annotation, ${key}, should be probabilities of states and add to 1.0`);
+                    }
+                } else if (typeof value === typeof true) {
+                    type = (type === undefined) ? Type.DISCRETE : type;
+
+                    if (type === Type.PROBABILITIES) {
+                        throw Error(`the values of annotation, ${key}, should be all boolean or all floats`);
+                    }
+                } else {
+                    throw Error(`the values of annotation, ${key}, should be all boolean or all floats`);
+                }
+                keys.push(key);
+            }
+            if (annotation.type && annotation.type !== type) {
+                throw Error(`existing values of the annotation, ${key}, in the tree is not of the same type`);
+            }
+            annotation.type = type;
+            annotation.values = annotation.values? [...annotation.values, addValues]:[addValues]
+        } else {
+            let type = Type.DISCRETE;
+
+            if (typeof addValues === typeof true) {
+                type = Type.BOOLEAN;
+            } else if (!isNaN(addValues)) {
+                type = (addValues % 1 === 0 ? Type.INTEGER : Type.FLOAT);
+            }
+
+            if (annotation.type && annotation.type !== type) {
+                if ((type === Type.INTEGER && annotation.type === Type.FLOAT) ||
+                    (type === Type.FLOAT && annotation.type === Type.INTEGER)) {
+                    // upgrade to float
+                    type = Type.FLOAT;
+                } else {
+                    throw Error(`existing values of the annotation, ${key}, in the tree is not of the same type`);
+                }
+            }
+
+            if (type === Type.DISCRETE) {
+                if (!annotation.values) {
+                    annotation.values = new Set();
+                }
+                annotation.values.add(addValues);
+            }else if( type===Type.FLOAT|| type===Type.INTEGER){
+                annotation.extent=[addValues,addValues]
+            }
+
+            annotation.type = type;
+        }
+
+        // overwrite the existing annotation property
+        // annotationTypes[key] = annotation;
+    }
+    return annotationTypes;
+}
--- tests/immutableTree.test.js
+++ tests/immutableTree.test.js
@@ -1,10 +1,73 @@
 import ImmutableTree from "../src/utils/immutableTree";
+import {Type} from "../src/utils/tree";
 
 describe("Tree Tests",()=>{
-    it("parse newick",()=>{
-        const treeString="((A:1,B:2):3,C:4,D242[&length_range={2.9997859591901488E-6,0.15966713321452264},location=\"Mabalako\",location.prob=1.0,location.set={\"Mabalako\"},8630136985685]:4)[&a={1}]):120;"
+    it("parse newick tree parse, type and reconcile annotations",()=>{
+        const treeString="((A:1,B[&length_range={1,1.5},location=\"Janesburgh\",location.prob={0.8,0.2},location.set={\"Janesburgh\",\"JanosAires\"}]:2):3,C[&length_range={2,4},location=\"Mabalako\",location.prob=1.0,location.set={\"Mabalako\"}]:4):120;"
         const tree = ImmutableTree.parseNewick(treeString);
 
-        console.log(tree);
+        expect(tree).toEqual({
+            nodesById: {
+                node2: {
+                    id: 'node2',
+                    name: null,
+                    label: null,
+                    length: 120,
+                    children: ["node1", "C"]
+                },
+                node1: {
+                    id: 'node1',
+                    name: null,
+                    length: 3,
+                    label: null,
+                    children: ["A", "B"]
+                },
+                C: {
+                    id: 'C',
+                    name: "C",
+                    length: 4,
+                    label: null,
+                    children: null
+                },
+                A: {
+                    id: 'A',
+                    name: "A",
+                    length: 1,
+                    label: null,
+                    children: null
+                },
+                B: {
+                    id: 'B',
+                    name: "B",
+                    length: 2,
+                    label: null,
+                    children: null
+                }
+            },
+            annotationsById:{
+                node2: {},
+                node1: {},
+                C: {
+                    length_range:[2,4],
+                    location:'Mabalako',
+                    location_prob:1.0,
+                    location_set:["Mabalako"],
+                    location_probSet:{"Mabalako":1}
+                },
+                A: {},
+                B: { length_range:[1,1.5],
+                    location:'Janesburgh',
+                    location_prob:[0.8,0.2],
+                    location_set:["Janesburgh","JanosAires"],
+                    location_probSet:{"Janesburgh":0.8,"JanosAires":0.2}}
+            },
+            annotationTypes:{
+                length_range:{type:Type.FLOAT,extent:[1,4]},
+                location:{type:Type.DISCRETE,values:new Set(["Janesburgh","Mabalako"])},
+                location_prob:{type:Type.FLOAT,extent:[0.2,1]},
+                location_set:{type:Type.DISCRETE,values:new Set(["Janesburgh","Mabalako"])},
+                location_probSet:{type:Type.PROBABILITIES,values:[{"Janesburgh":0.8,"JanosAires":0.2},{"Mabalako":1}]}
+            }
+        })
     })
 });
